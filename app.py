6f41955d3ab400f7deec06a0e68dea7cc7582f47
rou-miyazaki <s2232153bv@s.chibakoudai.jp>
2025-11-04 12:59:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -247,4 +247,4 @@ def index():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))  # Renderが割り当てるポートを取得
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
